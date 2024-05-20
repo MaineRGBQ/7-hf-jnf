60e30e970ab87668d2522a1ae52414fcbb3dfb49
MaineRGBQ <167762351+MaineRGBQ@users.noreply.github.com>
2024-05-20 12:42:42 +0400

    Add files via upload

---
+++ WpfApp32/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp32
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRunAway_MouseEnter(object sender, MouseEventArgs e)
+        {
+            Point buttonPosition = btnRunAway.TranslatePoint(new Point(0, 0), this);
+
+            double newX = buttonPosition.X + 50;
+            double newY = buttonPosition.Y + 50;
+
+            btnRunAway.Margin = new Thickness(newX, newY, 0, 0);
+        }
+    }
+
+}
